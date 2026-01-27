2d06821e43df2d7b8861cf0896b5db88ef032f46
Rohan Tondelkar <rohanvaibhavtondlekar@gmail.com>
2026-01-27 14:43:36 +0100

    Bump version to v0.1.2

--- zotero-plugin.config.ts
+++ zotero-plugin.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   name: pkg.config.addonName,
   id: pkg.config.addonID,
   namespace: pkg.config.addonRef,
-  updateURL: `https://github.com/forrtproject/fred_zotero/releases/download/release/${
+  updateURL: `https://forrtproject.github.io/fred_zotero/${
     pkg.version.includes("-") ? "update-beta.json" : "update.json"
   }`,
   xpiDownloadLink:
